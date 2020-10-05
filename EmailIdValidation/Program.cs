1f16602e3daef80bb0c93a8cf0563e3cd685a1d3
Rumana <rumanaparven16@gmail.com>
2020-10-05 19:33:34 +0530

    first commit

---
+++ EmailIdValidation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmailIdValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to emailID validation problem.");
+            
+
+        }
+    }
+}
